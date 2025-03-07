bce937c98eec0f1fd4bce6a42e813f01543af153
sonms <smsms5676@naver.com>
2025-03-07 14:29:33 +0900

    중앙정렬

--- pages/LoginPages.jsx
+++ pages/LoginPages.jsx
@@ -1,6 +1,8 @@
 import React from "react";
-import Form from "../components/form"
+import Form from "../components/form";
 import LoginButton from "../components/LoginButton";
+import styles from "./LoginPages.module.css"; 
+
 //import { useAuth } from "../hooks/useAuth";
 
 const LoginPages = () => {
@@ -11,10 +13,10 @@ const LoginPages = () => {
 //   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
-      <div className="w-96 p-6 bg-white shadow-md rounded-md">
+    // 해상도에 따라 md , lg 등이 존재
+    <div className={`min-h-screen bg-gray-100 ${styles.parent}`}> 
+      <div className={`w-96 p-6 bg-white shadow-md rounded-md ${styles.child}`}>
         <h2 className="text-2xl font-bold text-center mb-4">로그인</h2>
-      
         <LoginButton /> 
         <Form />
       </div>
