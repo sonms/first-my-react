d04df0f58600901deffea109818fd5a3235cc67b
sonms <smsms5676@naver.com>
2025-03-08 19:03:55 +0900

    카카오 세팅

--- pages/LoginPages.jsx
+++ pages/LoginPages.jsx
@@ -1,16 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Form from "../components/form";
 import LoginButton from "../components/LoginButton";
 import styles from "./LoginPages.module.css"; 
 
 //import { useAuth } from "../hooks/useAuth";
-
-const LoginPages = () => {
 //   const { login, loading, error } = useAuth();
 
 //   const handleLogin = async (credentials) => {
 //     await login(credentials);
 //   };
+
+function KakaoButton ({onClick}) {
+  const [email, setEmail] = useState("")
+}
+
+const LoginPages = () => {
+
+  const handleLogin = async (credentials) => {
+    console.log(credentials); // 콘솔에 출력
+    alert(JSON.stringify(credentials)); // alert 창에 JSON 문자열로 출력
+  };
 
   return (
     // 해상도에 따라 md , lg 등이 존재
@@ -18,7 +27,7 @@ const LoginPages = () => {
       <div className={`w-96 p-6 bg-white shadow-md rounded-md ${styles.child}`}>
         <h2 className="text-2xl font-bold text-center mb-4">로그인</h2>
         <LoginButton /> 
-        <Form />
+        <Form  onSubmit={handleLogin}/>
       </div>
     </div> 
   );
